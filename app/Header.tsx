77eec5dd0bc9420786ac84d67ca34f6e5ca0a456
elmergustavo <elmergustavo79@gmail.com>
2022-11-19 17:11:16 -0600

    component header

---
+++ app/Header.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 bg-white flex justify-center items-center p-10 shadow-sm">
+      <div className="flex flex-col items-center ">
+        <div className="flex space-x-2 items-center">
+          <Image
+            src="https://links.papareact.com/jne"
+            height={10}
+            width={50}
+            alt="Logo"
+          />
+          <p className="text-blue-400">Welcome to Meta Messenger</p>
+        </div>
+
+        <Link
+          href="/auth/signin"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Sign In
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Header from "./Header";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
