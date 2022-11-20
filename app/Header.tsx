a9da02e37aa9cff7db3bfa5db6e87b2fa9f983ef
elmergustavo <elmergustavo79@gmail.com>
2022-11-19 18:23:35 -0600

    update

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,8 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import LogoutButton from "./LogoutButton";
 
 function Header() {
+  const session = false;
+  if (session) {
+    return (
+      <header className="sticky top-0 z-50 bg-white flex justify-between items-center p-10 shadow-sm">
+        <div className="flex space-x-2">
+          <Image
+            className="rounded-full mx-2 object-contain"
+            src="https://links.papareact.com/jne"
+            height={10}
+            width={50}
+            alt="Logo"
+          />
+
+          <div>
+            <p className="text-blue-400">Logged in as:</p>
+            <p className="font-bold text-lg">Sonny Sangha</p>
+          </div>
+        </div>
+        <LogoutButton />
+      </header>
+    );
+  }
   return (
     <header className="sticky top-0 z-50 bg-white flex justify-center items-center p-10 shadow-sm">
       <div className="flex flex-col items-center ">
---
+++ app/LogoutButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LogoutButton() {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Sign Out
+    </button>
+  );
+}
+
+export default LogoutButton;
